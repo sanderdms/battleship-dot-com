223dd3d61f3b4ee27d42f65304622f4159615998
sanderdms <sanderdooms@live.be>
2020-01-22 13:50:03 +0100

    add test

--- battleship-sink-a-dot-com/src/SimpleDotComTestDrive.java
+++ battleship-sink-a-dot-com/src/SimpleDotComTestDrive.java
@@ -1,9 +1,24 @@
 
-public class SimpleDotComTestDrive extends SimpleDotCom {
-
+public class SimpleDotComTestDrive{
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Instantiate SimpleDotCom object
+		SimpleDotCom dot = new SimpleDotCom();	
+		// Assign a location
+		int[] locations = {2,3,3};
+		dot.setLocationCells(locations);
+		// Create a String to represent user input
+		String userGuess = "2";
+		// invoke checkYourself passing String
+		String result = dot.checkYourself(userGuess);
+		// set default testResult to failed
+		String testResult = "failed";
+		// Set testResult "passed" if the test outputs the expected "hit"
+		if(result.equals("hit")) {
+			testResult = "passed";
+		}
+		// Print out the test result
+		System.out.println(testResult);
 	}
-
+	
 }
